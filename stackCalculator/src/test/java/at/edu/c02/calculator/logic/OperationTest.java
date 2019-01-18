703540b3caeb124b51dcb3c03d6fc0ac96308ce6
baraga <baraga@UCL08956.knapp.at>
2019-01-18 15:53:26 +0100

    OperationTest Mod

--- stackCalculator/src/test/java/at/edu/c02/calculator/logic/OperationTest.java
+++ stackCalculator/src/test/java/at/edu/c02/calculator/logic/OperationTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import at.edu.c02.calculator.CalculatorException;
 import at.edu.c02.calculator.MathOperation;
 import at.edu.c02.calculator.logic.operations.Add;
+import at.edu.c02.calculator.logic.operations.Mod;
 
 public class OperationTest {
 
@@ -21,7 +22,23 @@ public class OperationTest {
 		
 		//verify
 		assertEquals(78, result, 0.0001);
+		
 	}
+	
+public void Mod() throws CalculatorException {
+		
+		//setup
+		MathOperation mod = new Mod();
+		
+		double result = mod.calculate(20, 7);
+		
+		//verify
+		assertEquals(6, result, 0.0001);
+		
+		
+	
+	}
+	
 
 
 	
